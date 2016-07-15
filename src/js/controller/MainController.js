a1d0c717ab814b515ec38ad4bb1ab552205b87e7
Punpikorn <sadanthem@gmail.com>
2016-07-15 14:25:06 +0900

    create image folder

--- src/js/controller/MainController.js
+++ src/js/controller/MainController.js
@@ -1,4 +1,39 @@
 angular.module('Kawaya', ['ui.router'])
   .controller('MainController', function () {
+  	//all user data
+  	var usersData = getUserData();
 
-  })
+
+  });
+
+function getUserData() {
+	return [
+	 { "userid":"100001", "name":"Tsuyoshi Yamamoto","sex":"M",
+	   "dob":"1953/12/16","contact":"1953/12/16"
+	 }
+	 { "userid":"100002", "name":"Kitta Takeshi","sex":"M",
+	   "dob":"1953/12/16","contact":"1953/12/16"
+	 }
+	 { "userid":"100003", "name":"Amano Maho","sex":"F",
+	   "dob":"1953/12/16","contact":"1953/12/16"
+	 }
+	 { "userid":"100005", "name":"Yamakawa Takuya","sex":"M",
+	   "dob":"1953/12/16","contact":"1953/12/16"
+	 }
+	 { "userid":"200001", "name":"Cynthia Paule","sex":"F",
+	   "dob":"1954/07/02","contact":"1954/07/02"
+	 }
+	 { "userid":"200002", "name":"Rebecca Rayborn","sex":"F",
+	   "dob":"1953/12/16","contact":"1953/12/16"
+	 }
+	 { "userid":"200003", "name":"Yamada Taro","sex":"M",
+	   "dob":"2000/01/01","contact":"2000/01/01"
+	 }
+	 { "userid":"200004", "name":"Yamada Taro","sex":"M",
+	   "dob":"2000/01/01","contact":"2000/01/01"
+	 }
+	 { "userid":"100004", "name":"山本 強","sex":"M",
+	   "dob":"1953/12/16","contact":"1953/12/16"
+	 }
+	];
+}

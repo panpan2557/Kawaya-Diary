e1797917b421198245a48cbc6cb574779ce4e2eb
Punpikorn <sadanthem@gmail.com>
2016-07-28 01:16:35 +0900

    finish the sort in user data

--- src/js/controller/UserEventController.js
+++ src/js/controller/UserEventController.js
@@ -1,31 +1,31 @@
 angular.module('Kawaya')
   .controller('UserEventController', ['$http', '$state', '$stateParams','$scope', '$rootScope',function ($http, $state, $stateParams,$scope,$rootScope) {
 
-      google.charts.setOnLoadCallback(drawChart);
+    google.charts.setOnLoadCallback(drawChart);
 
-      function drawChart() {
+    function drawChart() {
 
-        // Create the data table.
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Topping');
-        data.addColumn('number', 'Slices');
-        data.addRows([
-          ['Mushrooms', 3],
-          ['Onions', 1],
-          ['Olives', 1],
-          ['Zucchini', 1],
-          ['Pepperoni', 2]
-        ]);
+      // Create the data table.
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Topping');
+      data.addColumn('number', 'Slices');
+      data.addRows([
+        ['Mushrooms', 3],
+        ['Onions', 1],
+        ['Olives', 1],
+        ['Zucchini', 1],
+        ['Pepperoni', 2]
+      ]);
 
-        // Set chart options
-        var options = {'title':'How Much Pizza I Ate Last Night',
-                       'width':400,
-                       'height':300};
+      // Set chart options
+      var options = {'title':'How Much Pizza I Ate Last Night',
+                     'width':400,
+                     'height':300};
 
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
+      // Instantiate and draw our chart, passing in some options.
+      var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+      chart.draw(data, options);
+    }
 
     var controller = this;
     controller.userID = $stateParams.userID;
@@ -52,47 +52,44 @@ angular.module('Kawaya')
           controller.userdata = response.data;
       }, function errorCallback(response) {
      });
-  };
+    };
 
-  //wait for API to finish
+    //wait for API to finish
+    controller.getIndividual = function(userID){
+      url = 'https://crossorigin.me/' + 'http://ime.ist.hokudai.ac.jp/~yamamoto/kawaya/api-json-userdata.cgi?userid=' + userID;
+      $http({
+          method: 'GET',
+          url: url
+        }).then(function successCallback(response) {
+            controller.indv = response.data.userdata;
+            //for sorting
+            controller.dates = new Set();
+            controller.bristols = [1,2,3,4,5,6,7];
+            var j = 0;
+            for(var i=0; i < controller.indv.length; i++){
+              controller.dates.add(controller.indv[i].date);
+              controller.indv[i].indexID = j++;
+            }
+            controller.dates = Array.from(controller.dates);
+        }, function errorCallback(response) {
+       });
+    };
 
-  controller.getIndividual = function(userID){
-    url = 'https://crossorigin.me/' + 'http://ime.ist.hokudai.ac.jp/~yamamoto/kawaya/api-json-userdata.cgi?userid=' + userID;
-    $http({
-        method: 'GET',
-        url: url
-      }).then(function successCallback(response) {
-          controller.indv = response.data.userdata;
-          var j = 0;
-          for(var i=0;i<controller.indv.length;i++){
-            controller.indv[i].indexID = j++;
-          }
-        
-      }, function errorCallback(response) {
-     });
-  };
+    controller.getIndividual(controller.userID);
 
-  controller.getIndividual(controller.userID);
+    //init the bristol model first
+    $scope.select = {
+      bristol: ""
+    }
 
+    //custom filter the bristol
+    $scope.bristolFilter = function(event) {
+      //"" == Any
+      if ($scope.select.bristol == "")
+        return true;
+      return event.bristol === $scope.select.bristol;
+    };
 
-      controller.data = [
-       { "userid":"100001", "date":"2016/07/12","time":"14:01:58",
-         "bristol":"6","devid":"8964dc8240430f28",
-         "lat":"43.0546","lon":"141.341","ipaddr":"192.50.101.187"
-       },
-       { "userid":"100001", "date":"2016/07/12","time":"14:05:45",
-         "bristol":"2","devid":"8964dc8240430f28",
-         "lat":"43.0546","lon":"141.341","ipaddr":"192.50.101.187"
-       },
-       { "userid":"100001", "date":"2016/07/15","time":"04:35:00",
-         "bristol":"2","devid":"155eb38dc8cd8f2",
-         "lat":"43.0763316","lon":"141.3386866","ipaddr":"175.131.142.212"
-       },
-       { "userid":"100001", "date":"2016/07/15","time":"13:47:27",
-         "bristol":"5","devid":"155eb38dc8cd8f2",
-         "lat":"43.0763316","lon":"141.3386866","ipaddr":"133.87.132.68"
-       }
-      ];
   }
 ]);
 
